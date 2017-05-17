7b66dabdce859ae22788a24fab7360b180b09183
lislis <mail@lislis.de>
2017-05-17 14:59:10 +0200

    start building the request

--- app/components/trigger-custom-build.js
+++ app/components/trigger-custom-build.js
@@ -1,9 +1,35 @@
 import Ember from 'ember';
+import { task } from 'ember-concurrency';
+
+const { service } = Ember.inject;
 
 export default Ember.Component.extend({
+  ajax: service(),
+
+  triggerBuildMessage: '',
+  triggerBuildConfig: '',
+  triggerBuildBranch: '',
+
+  sendTriggerRequest() {
+    let body = {
+      request: {
+        message: this.get('triggerBuildMessage'),
+        branch: this.get('triggerBuildBranch'),
+        config: JSON.stringify(this.get('triggerBuildConfig'))
+      }
+    };
+    body = JSON.stringify(body);
+
+    console.log(body);
+    return this.get('ajax').postV3(`/repo/${this.get('repo.id')}/requests`, {})
+      .then(() => {
+        console.log('lol');
+      });
+  },
+
   actions: {
     triggerCustomBuild() {
-      console.log('TRIGGER');
+      this.sendTriggerRequest();
     }
   }
 });
