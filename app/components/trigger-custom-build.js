feb790eb3817faf17c9310dd7c8179d5e6353979
lislis <mail@lislis.de>
2017-05-17 17:02:32 +0200

    try a more verbose solution

--- app/components/trigger-custom-build.js
+++ app/components/trigger-custom-build.js
@@ -12,11 +12,17 @@ export default Ember.Component.extend({
 
   sendTriggerRequest() {
     let body = {};
-    body.request = {
-      message: this.get('triggerBuildMessage'),
-      branch: this.get('triggerBuildBranch'),
-      config: this.get('triggerBuildConfig')
-    };
+    body.request = {};
+
+    if (this.get('triggerBuildMessage') !== '') {
+      body.request.message = this.get('triggerBuildMessage');
+    }
+    if (this.get('triggerBuildConfig') !== '') {
+      body.request.config = this.get('triggerBuildConfig');
+    }
+    if (this.get('triggerBuildBranch') !== '') {
+      body.request.branch = this.get('triggerBuildBranch');
+    }
 
     body.request = JSON.stringify(body.request);
 
