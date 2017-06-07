ed8bd96e5c344c3ef8cf2d92c135e3c5754eeb68
lislis <mail@lislis.de>
2017-06-07 17:42:24 +0200

    add pagination component

---
+++ tests/integration/components/pagination-navigation-test.js
@@ -0,0 +1,29 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('pagination-navigation', 'Integration | Component | pagination navigation', {
+  integration: true
+});
+
+test('it renders list of pages', function (assert) {
+  let pagination = {
+    count: 34,
+    limit: 5,
+    offset: 0,
+    is_first: true,
+    is_last: false,
+    prev: null,
+    next: {
+      offset: 5,
+      limit: 5
+    }
+  };
+
+  this.set('pagination', pagination);
+  this.render(hbs`{{pagination-navigation pagination=pagination route="someroute"}}`);
+
+  assert.equal(this.$().find('li.pagination-button').count, 1, 'on the first page should have one navigation button');
+  assert.equal(this.$().find('li.pagination-button').text(), 'next', 'should have a next button on the first page');
+
+  assert.equal(this.$().find('li').count, 7, 'should calculate correct page count plus button');
+});
