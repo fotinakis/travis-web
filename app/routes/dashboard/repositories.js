f8ca91fc746da0435ae0b8e547454ed382491d00
lislis <mail@lislis.de>
2017-06-13 12:57:23 +0200

    create paginated-collection util

--- app/routes/dashboard/repositories.js
+++ app/routes/dashboard/repositories.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import TravisRoute from 'travis/routes/basic';
+import PaginatedCollection from 'travis/utils/paginated-collection';
 
 export default TravisRoute.extend({
   queryParams: {
@@ -18,13 +19,16 @@ export default TravisRoute.extend({
   },
 
   model(params) {
-    return Ember.RSVP.hash({
-      repos: this.store.query('repo', {
+    let collection = PaginatedCollection.create({
+      content: this.store.query('repo', {
         active: true,
         withLastBuild: true,
         sort_by: 'last_build.finished_at:desc',
         offset: params.offset
       }),
+    });
+    return Ember.RSVP.hash({
+      repos: collection,
       accounts: this.store.query('account', {
         all: true
       })
